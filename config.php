9f062cc2b1d6b4e7eb61a49fe10992ebdae4beef
YianAndCode <yian0418@gmail.com>
2014-10-14 23:48:12 +0800

    配置文件
    配置文件，填写密钥等相关信息

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+    /* 链接有效时间（秒），默认为10分钟 */
+    $available_time = 600;
+    /* upyun 中设置的密钥 */
+    $key = '';
+    /* 文件的相对路径，例如 /dir/pic.jpg */
+    $path = '';
+    /* 文件描述名 */
+    $filename = '';
+    /* upyun 空间的 URL ，以 / 结尾*/
+    $url = '';
+
+?>
